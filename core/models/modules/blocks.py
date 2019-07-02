a886a5d7bcdb203cf4e0ab707ec6c3a92e477a1b
shahine <shahine96@gmail.com>
2019-07-02 08:47:59 +0200

    added output size convblock util

--- core/models/modules/blocks.py
+++ core/models/modules/blocks.py
@@ -18,22 +18,40 @@ class Conv2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, relu=False, bn=False):
         super(Conv2d, self).__init__()
-        self._conv = nn.Conv2d(in_channels=in_channels,
-                               out_channels=out_channels,
-                               kernel_size=kernel_size,
-                               stride=stride,
-                               padding=padding,
-                               dilation=dilation)
-        self._bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
-        self._relu = nn.ReLU(inplace=True) if relu else None
+        self.conv = nn.Conv2d(in_channels=in_channels,
+                              out_channels=out_channels,
+                              kernel_size=kernel_size,
+                              stride=stride,
+                              padding=padding,
+                              dilation=dilation)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
+        self.relu = nn.ReLU(inplace=True) if relu else None
 
     def forward(self, x):
-        x = self._conv(x)
-        if self._bn:
-            x = self._bn(x)
-        if self._relu:
-            x = self._relu(x)
+        x = self.conv(x)
+        if self.bn:
+            x = self.bn(x)
+        if self.relu:
+            x = self.relu(x)
         return x
+
+    def output_size(self, input_size):
+        """Computes output size
+
+        Args:
+            input_size (tuple): (C_in, H_in, W_in)
+        """
+        _, H_in, W_in = input_size
+        C_out = self.conv.out_channels
+        kernel_size = self.conv.kernel_size[0]
+        padding = self.conv.padding[0]
+        stride = self.conv.stride[0]
+        H_out = (H_in - kernel_size + 2 * padding) / stride + 1
+        W_out = (W_in - kernel_size + 2 * padding) / stride + 1
+        return (C_out, H_out, W_out)
+
+
+
 
 
 class ConvTranspose2d(nn.Module):
@@ -53,19 +71,27 @@ class ConvTranspose2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  output_padding=0, dilation=1, relu=False, bn=False):
         super(ConvTranspose2d, self).__init__()
-        self._conv = nn.ConvTranspose2d(in_channels=in_channels,
-                                        out_channels=out_channels,
-                                        kernel_size=kernel_size,
-                                        stride=stride,
-                                        output_padding=output_padding,
-                                        dilation=dilation)
-        self._bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
-        self._relu = nn.ReLU(inplace=True) if relu else None
+        self.conv = nn.ConvTranspose2d(in_channels=in_channels,
+                                       out_channels=out_channels,
+                                       kernel_size=kernel_size,
+                                       stride=stride,
+                                       output_padding=output_padding,
+                                       dilation=dilation)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0, affine=True) if bn else None
+        self.relu = nn.ReLU(inplace=True) if relu else None
 
     def forward(self, x):
-        x = self._conv(x)
-        if self._bn:
-            x = self._bn(x)
-        if self._relu:
-            x = self._relu(x)
+        x = self.conv(x)
+        if self.bn:
+            x = self.bn(x)
+        if self.relu:
+            x = self.relu(x)
         return x
+
+    def output_size(self, input_size):
+        """Computes output size
+
+        Args:
+            input_size (tuple): (C, H_in, W_in)
+        """
+        raise NotImplementedError
