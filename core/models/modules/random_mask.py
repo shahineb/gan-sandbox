e58352798f9ca1449bd19f0d97b30c6228c36a69
shahine <shahine96@gmail.com>
2019-07-21 22:05:05 +0200

    rectangular mask generation

--- core/models/modules/random_mask.py
+++ core/models/modules/random_mask.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -10,18 +11,27 @@ class FeatureMasksGenerator(nn.Module):
         self.min_coverage = coverage[0]
         self.max_coverage = coverage[1]
 
-    def random_coverage(self):
-        p = (self.max_coverage - self.min_coverage) * torch.rand(1).item() + self.min_coverage
-        return p
+    def rectangular_mask(self):
+        width, height = self.size
+        # Compute random ratios s.t min_coverage * w * h <= ratio * w * h <= max_coverage * w * h
+        width_ratio = np.sqrt(self.min_coverage) + (np.sqrt(self.max_coverage) - np.sqrt(self.min_coverage)) * np.random.rand()
+        height_ratio = np.sqrt(self.min_coverage) + (np.sqrt(self.max_coverage) - np.sqrt(self.min_coverage)) * np.random.rand()
+
+        # Derive mask dimensions and origin
+        mask_width = int(np.floor(width * width_ratio))
+        mask_height = int(np.floor(height * height_ratio))
+        x0 = int(np.floor((width - mask_width) * np.random.rand()))
+        y0 = int(np.floor((height - mask_height) * np.random.rand()))
+
+        # Build mask as (width, height) tensor
+        mask = torch.ones(self.size)
+        mask[x0:x0 + mask_width, y0:y0 + mask_height] = 0.
+        return mask
 
     def forward(self, batch_size=1):
-        # Generate available features mask
-        p = self.random_coverage()
-        a = torch.rand(self.size) > p
-
-        # Generate required features mask
-        p = self.random_coverage()
-        r = torch.rand(self.size) > p
+        # Generate available and required features masks
+        a = self.rectangular_mask()
+        r = self.rectangular_mask()
 
         # Zero out common features in required features
         r = (r.float() - a.float()) > 0
